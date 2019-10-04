0077f7070c871703dd250c66b560fab8ad133f6a
rsvenkata <sv_raghunath@hotmail.com>
2019-10-04 14:20:46 +0100

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -13,16 +13,13 @@ export default class home extends Component {
       Dabbara
       <span >Nagarjuna</span>
     </h1>
-    <h2 className="line-1 anim-typewriter"> Full-stack Developer & designer.</h2>
-    <p><strong>| HTML5 | CSS3 | SAAS | Bootstrap | JavaScript | React | Angular 8 | React & Angular Material Design | NodeJS | Express | MongoDB</strong></p>
+    <h2 className="line-1 anim-typewriter"> Full-stack Developer & Designer.</h2>
+    <p><strong> HTML5 | CSS3 | SAAS | Bootstrap | JavaScript | React | Angular 8 | React & Angular Material Design | NodeJS | Express | MongoDB</strong></p>
     <div className="icons">
-      <a href="#!">
-        <i className="fa fa-twitter fa-2x"></i>
-      </a>
       <a href="https://www.facebook.com/dnagarjuna.naidu.3">
         <i className="fa fa-facebook fa-2x"></i>
       </a>
-      <a href="#!">
+      <a href="https://www.linkedin.com/in/nagarjuna-chowdary-525451194">
         <i className="fa fa-linkedin fa-2x"></i>
       </a>
       <a href="https://github.com/arjunchowdaryd">
