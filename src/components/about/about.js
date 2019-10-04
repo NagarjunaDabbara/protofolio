6ec4b86955fb139edaa1de258431303f433f0722
rsvenkata <sv_raghunath@hotmail.com>
2019-10-04 19:34:49 +0100

    Update about.js

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -21,25 +21,24 @@ export default class About extends Component {
 
       <div className="bio">
       
-        <p>I am interested in IT and everything in its orbit. I recently began to be fascinated by web programming, e.g.building ReactJS websites. Invited to join my uncle's start-up company as a Full-Stack Developer, I gained experience of working in this area. As this area complements my studies, I am keen to gain more experience in the field. For this reason, I am looking for a company willing to offer me a placement among their developers. In return, I would offer my full commitment, and be a pleasant and friendly addition to your team.</p>
+        <p>I am interested in IT and everything in its orbit. I am fascinated by web programming, e.g.building ReactJS websites using Redux. I was invited to join my uncle's start-up company as a Full-Stack Developer, I gained experience of working in designing devoloping web applications. Now I am keen to gain more experience in this field. For this reason, I am looking for a company willing to offer me a placement among their developers' team. In return, I would offer my full commitment, and be a pleasant and friendly addition to your team.</p>
       </div>
 
       <div className="job job-1">
         <h3>Employee</h3>
         <h6>Full Stack Developer</h6>
-        <p>I worked in many Websites and also desing many pages.I worked in Modern tech like Loopback,Swagger and many More.</p>
+        <p>I worked on many Websites. I worked in Modern tech like Loopback, Swagger and many More.</p>
       </div>
 
       <div className="job job-2">
         <h3>Online Working</h3>
         <h6>Front End Developer</h6>
-        <p>Alot of worked in to desing many pages and websites for Interactive Wings and Worked on many tools and new tech to used in Front-end-Development.</p>
+        <p>I worked on interactive and responsive websites using many tools and new technology both on front end and bacn end development.</p>
       </div>
 
       <div className="job job-3">
         <h3>Online Working</h3>
         <h6>Front End Developer</h6>
-        <p>Alot of worked in to desing many pages and websites for Interactive Wings and Worked on many tools and new tech to used in Front-end-Development.</p>
       </div>
       </div>
   </main>
