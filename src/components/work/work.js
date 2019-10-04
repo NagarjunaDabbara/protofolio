8270412f7141dac973c1500fecadb3dae2d451d8
rsvenkata <sv_raghunath@hotmail.com>
2019-10-04 19:52:41 +0100

    Update work.js

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -51,17 +51,6 @@ export default class Work extends Component {
           <i className="fa fa-github"></i> Github
         </a>
       </div>
-      <div className="item">
-        <a href="#!">
-          <img src={arjun} alt="Project"/>
-        </a>
-        <a href="#" className="btn-light">
-          <i className="fa fa-eye"></i> Project
-        </a>
-        <a href="#" className="btn-dark">
-          <i className="fa fa-github"></i> Github
-        </a>
-      </div>
       
     </div>
   </main>
