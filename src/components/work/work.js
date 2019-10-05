3f699cc99cdb877de9b7c995ef3842e6e647a003
arjunchowdaryd <34588657+arjunchowdaryd@users.noreply.github.com>
2019-10-05 15:55:24 +0100

    Update work.js

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -9,6 +9,15 @@ export default class Work extends Component {
                 <Nav/>
     <h1 className="lg-heading">My<span className="text-secondary">Work</span></h1>
     <h2 className="sm-heading">Check out some of my projects...</h2>
+                
+                  <div className="Projects">   
+      <div className="item">
+        <a href="#!"> <img src={arjun} alt="Project"/> </a>
+        <a href="#" className="btn-light"> <i className="fa fa-eye"></i> Project</a>
+        <a href="#" className="btn-dark"> <i className="fa fa-github"></i> Github </a>
+      </div>
+      </div>
+                
               </div>)
     }
 }
