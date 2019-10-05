9657d9c329639c301b4e6802dcbad0ea63b228de
arjunchowdaryd <34588657+arjunchowdaryd@users.noreply.github.com>
2019-10-05 15:15:48 +0100

    Update work.js

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -6,33 +6,7 @@ import arjun from "../../assets/img/portrait1.jpg"
 export default class Work extends Component {
     render() {
         return (
-            <div>
-                <Nav/>
-                <div id="main">
-                <main id="work">
-    <h1 className="lg-heading">
-      My
-      <span className="text-secondary">Work</span>
-    </h1>
-    <h2 className="sm-heading">
-      Check out some of my projects...
-    </h2>
             
-    <div className="Projects">
-      
-      <div className="item">
-        <a href="#!"> <img src={arjun} alt="Project"/> </a>
-        <a href="#" className="btn-light"> <i className="fa fa-eye"></i> Project</a>
-        <a href="#" className="btn-dark"> <i className="fa fa-github"></i> Github </a>
-      </div>
-           
-      </div>
-      
-    </div>
-  </main>
-  </div>
-     <Footer/>
-        </div>
         )
     }
 }
