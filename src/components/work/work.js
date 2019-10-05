e59ffefd56e76e6770617351ba175f0826567052
arjunchowdaryd <34588657+arjunchowdaryd@users.noreply.github.com>
2019-10-05 16:05:33 +0100

    Update work.js

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -10,14 +10,21 @@ export default class Work extends Component {
     <h1 className="lg-heading">My<span className="text-secondary">Work</span></h1>
     <h2 className="sm-heading">Check out some of my projects...</h2>
                 
-                  <div className="Projects">   
+   <div className="Projects">   
       <div className="item">
         <a href="#!"> <img src={arjun} alt="Project"/> </a>
-        <a href="#" className="btn-light"> <i className="fa fa-eye"></i> Project</a>
-        <a href="#" className="btn-dark"> <i className="fa fa-github"></i> Github </a>
-      </div>
+        <a href="https://ananyaeliteproject.herokuapp.com/" className="btn-light"> <i className="fa fa-eye"></i> Project</a>
+        <a href="https://github.com/arjunchowdaryd/ananyaengineers" className="btn-dark"> <i className="fa fa-github"></i> Github </a>
       </div>
                 
-              </div>)
+      <div className="item">
+        <a href="#!"> <img src={arjun} alt="Project"/> </a>
+        <a href="https://beachproject.herokuapp.com/" className="btn-light"> <i className="fa fa-eye"></i> Project</a>
+        <a href="https://github.com/arjunchowdaryd/beachresort" className="btn-dark"> <i className="fa fa-github"></i> Github </a>
+      </div>
+                
+                
+    </div>             
+   </div>)
     }
 }
